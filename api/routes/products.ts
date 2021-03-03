dd1752aa69fe26c592b950c7feb5026d00583463
yuto-moriizumi <moriizumi.yuto.18@shizuoka.ac.jp>
2021-03-03 13:04:03 +0900

    amazon.comから価格取得する関数を実装

--- api/routes/products.ts
+++ api/routes/products.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import mysql2 from "mysql2/promise";
+import { JSDOM } from "jsdom";
+import axios from "axios";
 
 const router = express.Router();
 
@@ -17,19 +19,64 @@ interface Product {
   url_kakaku: string;
 }
 
-interface ProductWithID extends Product {
+interface ExProduct extends Product {
   id: number;
+  price_com: number;
+  price_kakaku: number;
 }
 
 // 製品を全て取得
 router.get("/", async (req, res) => {
   const connection = await mysql2.createConnection(DB_SETTING);
-  await connection.connect();
-  const [result, fields] = await connection.query("SELECT * FROM products");
-  const products = result as ProductWithID[];
-  res.send(products);
+  try {
+    await connection.connect();
+    const [result, fields] = await connection.query("SELECT * FROM products");
+    const products = result as ExProduct[];
+    res.send(
+      await Promise.all(
+        products.map(async (product) => {
+          product.price_com = await getAmazonPrice(product.url_com);
+          product.price_kakaku = -1;
+          return product;
+        })
+      )
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
   connection.end();
 });
+
+async function getAmazonPrice(url: string) {
+  try {
+    const url_object = new URL(url);
+  } catch (error) {
+    return -1;
+  }
+  const res = await axios.get(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0",
+    },
+  });
+
+  const document = new JSDOM(res.data).window.document;
+  const priceblock_ourprice = document.getElementById("priceblock_ourprice");
+  if (!priceblock_ourprice) return -1;
+  const ourprice_shippingmessage = document.getElementById(
+    "ourprice_shippingmessage"
+  );
+  if (!ourprice_shippingmessage) return -1;
+  //[Shipping & Import Fees Deposit, Price, AmazonGlobal Shipping, Estimated Import Fees Deposit, Total]
+  const pricesWith$ = ourprice_shippingmessage.textContent?.match(/\$[\d\.]*/g);
+  console.log(priceblock_ourprice.textContent, pricesWith$);
+  if (!pricesWith$) return -1;
+  const prices = pricesWith$.map((price) => {
+    return parseFloat(price.slice(1));
+  });
+  return prices[1] + prices[2];
+}
 
 // 商品を登録
 router.post("/", async (req, res) => {
